718a6bbb4d59c9a5a05d9fab80245aeb6245d81e
gilbertdev <83252067+Gilbert-mo@users.noreply.github.com>
2023-06-29 16:08:13 -0400

    cambiar icono de whatsApp

--- src/components/contactInfo/InfoWhatsApp.jsx
+++ src/components/contactInfo/InfoWhatsApp.jsx
@@ -6,7 +6,8 @@ function InfoWhatsApp({ text, phone }) {
     <div className="flex py-3 items-center hover:bg-gray-600 container-ws cursor-pointer w-full">
       <div className="w-10 h-10 shrink-0 text-2xl flex justify-center items-center ml-2 mr-2">
         <span className="span">
-          <FaWhatsappSquare />
+          {/* <FaWhatsappSquare /> */}
+          <img src="/ws.png" alt="logo-whatsApp" className="object-cover"/>
         </span>
       </div>
       <div className="flex flex-col phone">
